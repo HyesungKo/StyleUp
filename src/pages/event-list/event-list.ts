ef385d96fa17f2f686218d5e419d2b91880e5dd8
Hyesung Ko <kohs0429@gmail.com>
2017-11-15 04:27:18 -0800

    Search bar and dividing posts into normal and boutique working!

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -11,35 +11,72 @@ import { User } from 'firebase/app';
   templateUrl: 'event-list.html',
 })
 export class EventListPage {
-  public foo: any;
+  public normalList: any;
+  public boutiqueList: any;
   public posts: firebase.database.Reference;
   public postType = 'standard';
   
 
   constructor(public navCtrl: NavController) {
-    // this.currentUser = firebase.auth().currentUser.uid;
     this.posts = firebase.database().ref(`posts`);
   }
  
   ionViewDidEnter(){
     this.posts.on('value', snapshot => {
-      let rawList = [];
+      let normalList = [];
+      let boutiqueList = [];
       snapshot.forEach( snap => {
-        rawList.push({
-          id: snap.key,
-          eventLocation: snap.val().name,
-          photo: snap.val().photo,
-          eventCaption: snap.val().caption,
-          eventHashtag: snap.val().hashtags,
-          userType: snap.val().userTyle
-        });
+        if (snap.val().userType === 'regular'){
+          normalList.push({
+            id: snap.key,
+            eventLocation: snap.val().name,
+            photo: snap.val().photo,
+            eventCaption: snap.val().caption,
+            eventHashtag: snap.val().hashtags,
+            userType: snap.val().userType
+          });
+        } else {
+          boutiqueList.push({
+            id: snap.key,
+            eventLocation: snap.val().name,
+            photo: snap.val().photo,
+            eventCaption: snap.val().caption,
+            eventHashtag: snap.val().hashtags,
+            userType: snap.val().userType
+          });
+        }
+       
       return false
       });
-      this.foo = rawList;
+      this.normalList = normalList;
+      this.boutiqueList = boutiqueList;
     });
   }
 
   goToEventDetail(eventId){
     this.navCtrl.push('EventDetailPage', { eventId: eventId });
+  }
+
+  
+  filterPost(ev: any) {
+    this.ionViewDidEnter();
+    if(this.postType === 'standard'){
+      let val = ev.target.value;
+  
+      if (val && val.trim() !== '') {
+        this.normalList = this.normalList.filter(function(item) {
+          return item.eventCaption.toLowerCase().includes(val.toLowerCase());
+        });
+      }
+    } else {
+      let val = ev.target.value;
+      
+      if (val && val.trim() !== '') {
+        this.boutiqueList = this.boutiqueList.filter(function(item) {
+          return item.eventCaption.toLowerCase().includes(val.toLowerCase());
+        });
+      }
+    }
+    
   }
 }

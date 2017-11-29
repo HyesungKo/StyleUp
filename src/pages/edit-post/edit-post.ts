9f3684f3a90468c4b5a922128c8349df7ed0dfbb
Hyesung Ko <kohs0429@gmail.com>
2017-11-29 14:20:39 -0800

    modified post editting page, integrating with location page

--- src/pages/edit-post/edit-post.ts
+++ src/pages/edit-post/edit-post.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import firebase from 'firebase';
 
 @IonicPage()
@@ -14,14 +14,16 @@ export class EditPostPage {
   public currentUser: string;
   public currentPostRef: firebase.database.Reference;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private modal: ModalController) {
     this.currentUser = firebase.auth().currentUser.uid;
     this.currentPost = this.navParams.get('currentPost');
     console.log(this.currentPost);
-    
     this.currentPostRef = firebase.database().ref(`posts/${this.currentPost.id}`);
   }
 
+  /* 
+  * Update
+  */
   updatePost(location : string, caption : string, hashtags: string) {
     this.currentPostRef.update({
       "name" : location,
@@ -36,6 +38,15 @@ export class EditPostPage {
     this.currentPostRef.remove();
 
     this.navCtrl.setRoot('ProfilePage');
+  }
+
+  openLocationModal() {
+    const myModal = this.modal.create('LocationPage', { city: this.currentPost.location }); //passes current value of location in case user cancels
+    myModal.present();
+
+    myModal.onDidDismiss((item) => {
+      this.currentPost.location = item.description; //sets new location
+    });
   }
 
 }
